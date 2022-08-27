2ee589a64e7b5e2527a80b69a6f55318075dbe5c
Kishan <Kishanprasad09102001@gmail.com>
2022-08-27 16:19:32 +0530

    Changes made in Banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
           <Image src={"https://bit.ly/3TiJzPh"} alt="" layout="fill" objectFit='cover' />
           <div className="absolute top-1/2 w-full text-center">
               <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-              <button className="text-sm sm:text-lg text-purple-500 bg-white px-6 sm:px-9 py-2 sm:py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm flexiable</button>
+              <button className="text-sm sm:text-lg text-purple-500 bg-white px-6 sm:px-9 py-2 sm:py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I&apos;m flexiable</button>
           </div>
       </section>
   )
